00b7f41f083faf03bae164fe75606af689c89f99
shreyyg <shreyag@andrew.cmu.edu>
2016-10-30 14:30:15 -0400

    Add files via upload

---
+++ mp32mfcc.m
@@ -0,0 +1,12 @@
+[speech,fs] = audioread('song.wav'); %insert song here
+length = 273000; %length of sample in ms
+Tw = length/510;
+Ts = Tw*.5;
+alpha = .95;
+M = 22;
+L = 22;
+window = blackmanharris(32);
+R = [22 22000];
+N = 22;
+speech = (speech(:,1)+speech(:,2))/2; %convert from stereo to audio
+[CC,FBE,frames] =mfcc(speech,fs,Tw,Ts,alpha,window,R,M,N,L);
